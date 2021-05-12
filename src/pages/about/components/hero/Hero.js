25b3571419fa1c4329f0ba62909e5e212c0faa3c
usman <usman.muhammad@piri.ai>
2021-05-12 17:12:07 +0500

    import image

--- src/pages/about/components/hero/Hero.js
+++ src/pages/about/components/hero/Hero.js
@@ -1,30 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Image from '../animated-image/Image';
 import Arrow from './Arrow';
-import boy1 from './images/boy1.png';
-import boy2 from './images/boy2.png';
-import boy3 from './images/boy3.png';
-import boy4 from './images/boy4.png';
-import girl1 from './images/girl1.png';
-import girl2 from './images/girl2.png';
-import girl3 from './images/girl3.png';
-import girl4 from './images/girl4.png';
 import style from './style.module.css';
-
-const images = {
-  girl: {
-    1: girl1,
-    2: girl2,
-    3: girl3,
-    4: girl4,
-  },
-  boy: {
-    1: boy1,
-    2: boy2,
-    3: boy3,
-    4: boy4,
-  },
-};
 
 const Hero = () => {
   const [boyCount, setBoyCount] = useState(1);
@@ -69,15 +46,16 @@ const Hero = () => {
     <>
       <Image
         handleMouse={(leave) => handleMouse('boy', leave)}
-        url={mouseOver[0] ? images.boy['1'] : images.boy[boyCount]}
-        // url={`${process.env.PUBLIC_URL}/assets/images/boy${
-        //   mouseOver[0] ? 1 : boyCount
-        // }.png`}
+        url={`${process.env.PUBLIC_URL}/assets/images/boy${
+          mouseOver[0] ? 1 : boyCount
+        }.png`}
         pos={{ right: `${pos}%` }}
       />
       <Image
         handleMouse={(leave) => handleMouse('girl', leave)}
-        url={mouseOver[1] ? images.girl['1'] : images.girl[girlCount]}
+        url={`${process.env.PUBLIC_URL}/assets/images/girl${
+          mouseOver[1] ? 1 : girlCount
+        }.png`}
         pos={{ left: `${pos + 10}%` }}
       />
       <div
